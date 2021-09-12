81328bd5b284ebfe453ae8d1fa2352a6016c2d53
Rafael @ Anakin <rafajs7@gmail.com>
2021-09-12 16:50:47 +0100

    Added Cart Provider

--- src/views/Base.js
+++ src/views/Base.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { CartProvider } from "react-use-cart";
 import { FooterBar } from "@components";
 
 export function Base(props) {
     return (
-        <div className="md:p-10 md:pb-0 flex flex-col h-screen justify-between bg-base-200">
-            {props.children}
-            <FooterBar/>
-        </div>
+        <CartProvider>
+            <div className="md:p-10 md:pb-0 flex flex-col h-screen justify-between bg-base-200">
+                {props.children}
+                <FooterBar/>
+            </div>
+        </CartProvider>
     );
 }
