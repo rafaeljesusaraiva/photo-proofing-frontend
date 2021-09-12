7426d7dda5c1862ae6b4eb3c9f21fb9cb17abee3
Rafael @ Anakin <rafajs7@gmail.com>
2021-09-13 00:40:12 +0100

    Updated Card to receive props

--- src/components/layout/custom/Card.js
+++ src/components/layout/custom/Card.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function Card(props) {
+    const cardInfo = props.info;
     return (
-        <div className={"card shadow-xl cursor-pointer image-full md:transform hover:translate-x-1 hover:-translate-y-1 transition-transform duration-250 ease-out"+props.className}>
-            <figure>
-                <img src="https://picsum.photos/id/1005/400/250"/>
-            </figure> 
-            <div className="justify-end card-body h-full">
-                <h2 className="card-title select-none">Image overlay</h2> 
-                {/* <div className="card-actions">
-                    <button className="btn btn-primary btn-sm">Get Started</button>
-                </div> */}
-            </div>
-        </div> 
+        <Link to={cardInfo.url}>
+            <div className={"card shadow-xl cursor-pointer image-full md:transform hover:translate-x-1 hover:-translate-y-1 transition-transform duration-250 ease-out"+props.className}>
+                <figure>
+                    <img src={cardInfo.cover}/>
+                </figure> 
+                <div className="justify-end card-body h-full">
+                    <h2 className="card-title select-none">{cardInfo.name}</h2> 
+                </div>
+            </div> 
+        </Link>
     );
 }
