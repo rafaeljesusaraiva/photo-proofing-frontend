724c6d2c21e1ead9b0244c5ccd8ac3917f62bc69
Rafael @ Anakin <rafajs7@gmail.com>
2021-09-16 02:47:51 +0100

    Updated Admin Pages Layout

--- src/views/Admin/PhotoSizes/Main.PhotoSizes.js
+++ src/views/Admin/PhotoSizes/Main.PhotoSizes.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { AdminBar, NavBar } from "@components";
 
 export function Main(){
     return (
-        <div>Index</div>
+        <>
+            <NavBar/>
+            <AdminBar/>
+            <h2 className="text-4xl mx-6 md:mx-0 my-4 select-none">Administração</h2>
+            <div className="overflow-x-hidden flex-grow w-full md:mx-auto my-4">
+                <div className="w-full">Página</div>
+            </div>
+        </>
     );
 }
