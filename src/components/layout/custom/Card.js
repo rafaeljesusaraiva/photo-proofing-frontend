5d382ff0690c1ce2af4c03acffce92ee419fd54e
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2022-01-22 22:46:22 +0000

    Added Modal warning user to login/register to see event

--- src/components/layout/custom/Card.js
+++ src/components/layout/custom/Card.js
@@ -1,13 +1,35 @@
 import React from "react";
 import { ConditionalLink } from "@components";
+import { Authentication } from '@services';
 
 export function Card(props) {
     const cardInfo = props.info;
+    const currentUser = Authentication.currentUserValue;
+
+
+    const warningNoLogin = () => {
+        console.log('yo')
+        props.setShowModal(cardInfo.name);
+    }
+
+    if (!currentUser) {
+        return (
+            <div className={"relative card shadow-xl cursor-pointer md:transform hover:translate-x-1 hover:-translate-y-1 transition-transform duration-250 ease-out"+props.className} onClick={warningNoLogin}>
+                <figure>
+                    <img src={cardInfo.cover} alt="Album Cover"/>
+                </figure> 
+                <div className="absolute bottom-0 left-0 card-body p-2 px-4 bg-black/75 w-full">
+                    <h2 className="card-title select-none mb-0">{cardInfo.name}</h2> 
+                </div>
+            </div> 
+        )
+    }
+    
     return (
         <ConditionalLink to={cardInfo.url} valid={props.valid}>
             <div className={"relative card shadow-xl cursor-pointer md:transform hover:translate-x-1 hover:-translate-y-1 transition-transform duration-250 ease-out"+props.className}>
                 <figure>
-                    <img src={cardInfo.cover}/>
+                    <img src={cardInfo.cover} alt="Album Cover"/>
                 </figure> 
                 <div className="absolute bottom-0 left-0 card-body p-2 px-4 bg-black/75 w-full">
                     <h2 className="card-title select-none mb-0">{cardInfo.name}</h2> 
--- src/views/Public/IndexPage.js
+++ src/views/Public/IndexPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Api } from "@services";
 import { Card, NavBar } from "@components";
 import { DateInBetween } from "@utils";
@@ -8,11 +9,16 @@ export function Index() {
     document.title = `Início | Rafael Jesus Saraiva`;
     
     const [albums, setAlbums] = useState([]);
-    useEffect(async ()=>{
-        if (albums.length === 0) {
-            setAlbums(await Api.getAlbums());
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(()=>{
+        async function execute() {
+            if (albums.length === 0) {
+                setAlbums(await Api.getAlbums());
+            }
         }
-    }, [])
+        execute();
+    }, [albums])
 
     return (
         <>
@@ -33,8 +39,20 @@ export function Index() {
                         url: "/prova/" + singleAlbum.slug,
                         cover: process.env.REACT_APP_DATABASE_URL + singleAlbum.cover
                     }
-                    return <Card key={'album-'+index} className="break-inside max-h-80 md:max-h-35v" valid={isValid} info={albumInfo}/>
+                    return <Card key={'album-'+index} className="break-inside max-h-80 md:max-h-35v" valid={isValid} info={albumInfo} setShowModal={setShowModal} showModal={showModal}/>
                 })}
+            </div>
+            <div className={"modal"+ (showModal ? " modal-open" : "")}>
+                <div className="modal-box">
+                    <h2 className="text-xl font-bold mb-4">AVISO</h2>
+                    <p>Para visualizar o evento "<b>{showModal}</b>" é necessário inscrever ou fazer login.</p> 
+                    <div className="modal-action">
+                        <Link to="/login">
+                            <label className="btn">Inscrever/Login</label> 
+                        </Link>
+                        <label onClick={()=>setShowModal(false)} className="btn btn-primary">Fechar Aviso</label> 
+                    </div>
+                </div>
             </div>
         </>
     );
